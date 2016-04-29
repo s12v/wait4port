2e2d550d9caaea957f155cb595fa8dc037077d6c
Sergey Novikov <snov@snov.me>
2016-04-29 23:35:16 +0200

    Improve output

--- wait4port.sh
+++ wait4port.sh
@@ -11,7 +11,7 @@ try_address() {
 	local port=${arr[1]}
 	if [ ! -z "$host" ] && [[ $port =~ ^[0-9]+$ ]]; then
 		local c=0
-		while ! nc -z "$host" "$port"; do
+		while ! nc -z "$host" "$port" 2> /dev/null; do
 			if [ "$c" -ge $RETRIES ]; then exit 1; fi
 			echo -n "."
 		  	sleep $DELAY
@@ -23,6 +23,7 @@ try_address() {
 	fi
 }
 
+echo -n "Waiting for TCP connections "
 commands=""
 pids=""
 for address; do
@@ -36,13 +37,15 @@ ret=0
 for p in $pids; do
 	if wait $p; then
 		tput setaf 2
-		echo -n "${commands[$p]}: ok "
+		echo -n " ${commands[$p]} up! "
+		tput sgr0
 	else
 		if [ "$?" -gt 128 ]; then
 		kill $?
 		else
 			tput setaf 1
-			echo -n "${commands[$p]}: timeout "
+			echo -n " ${commands[$p]} timeout "
+			tput sgr0
 		fi
 		ret=1
 	fi
