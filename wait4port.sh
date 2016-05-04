a5284a7c69dee7ae331d828eff68f7a82e2a5c25
Sergey Novikov <snov@snov.me>
2016-05-04 15:15:04 +0200

    Check http with curl

--- wait4port.sh
+++ wait4port.sh
@@ -1,26 +1,48 @@
 #!/usr/bin/env bash
-trap : SIGTERM SIGINT
+
+trap term SIGTERM SIGINT
 
 RETRIES=120
 DELAY=0.5
 
-try_address() {
-	arr=(${1//:/ })
+term() { 
+	echo "Caught SIGTERM/SIGINT signal!" 
+	for p in $pids; do
+		kill $p
+	done  
+}
 
-	local host=${arr[0]}
-	local port=${arr[1]}
-	if [ ! -z "$host" ] && [[ $port =~ ^[0-9]+$ ]]; then
-		local c=0
-		while ! nc -z "$host" "$port" 2> /dev/null; do
-			if [ "$c" -ge $RETRIES ]; then exit 1; fi
-			echo -n "."
-		  	sleep $DELAY
-		  	c=$((c + 1))
-		done
+try_address() {
+	local address="$1"
+	IFS=':' read -ra arr <<< "$address"
+	local m1=${arr[0]}
+	local m2=${arr[1]}
+	local m3=${arr[2]}
+
+	if [[ $m1 =~ ^http.?$ ]] && [ ! -z "$m2" ] && [[ $m3 =~ ^[0-9]+$ ]]; then
+		local cmd="curl -s -o /dev/null $m1:$m2:$m3"
+		tput setaf 4; echo -n "curl $address "; tput sgr0
+	elif [ ! -z "$m1" ] && [[ $m2 =~ ^[0-9]+$ ]]; then
+		local cmd="nc -z $m1 $m2"
+		tput setaf 5; echo -n "nc $address "; tput sgr0
 	else
-		echo "Invalid parameters: $host:$port"
+		echo "Invalid parameters: $address"
 		exit 1
 	fi
+
+	if [ ! -z "$cmd" ]; then
+		for i in `seq 1 $RETRIES`; do
+			if $cmd 2> /dev/null; then
+				tput setaf 2; echo -n " $address up "; tput sgr0
+				exit 0;
+			fi
+			echo -n "."
+		  	sleep $DELAY
+		done
+	fi
+
+	tput setaf 1; echo -n " $address timeout "; tput sgr0
+	exit 1
 }
 
 echo -n "Waiting for TCP connections "
@@ -30,22 +52,12 @@ for address; do
 	( try_address $address ) & pid=$!
 	commands[$pid]=$address
 	pids+=" $pid"
+	sleep 0.1
 done
 
 ret=0
 for p in $pids; do
-	if wait $p; then
-		tput setaf 2
-		echo -n " ${commands[$p]} up! "
-		tput sgr0
-	else
-		if [ "$?" -gt 128 ]; then
-			kill $?
-		else
-			tput setaf 1
-			echo -n " ${commands[$p]} timeout "
-			tput sgr0
-		fi
+	if ! wait $p; then
 		ret=1
 	fi
 done
